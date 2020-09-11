efc2af8ea6e4fb8f1a0d89b190a74e6b26486606
k.struzhkin <k.struzhkin@MacBook-Pro-rbkadmin.local>
2020-09-11 16:36:40 +0300

    Add new service

---
+++ proto/base.thrift
@@ -0,0 +1,4 @@
+namespace java com.rbkmoney.damsel.party_shop
+namespace erlang party_shop
+
+typedef string ID
---
+++ proto/party_shop.thrift
@@ -0,0 +1,16 @@
+include "base.thrift"
+
+namespace java com.rbkmoney.damsel.party_shop
+namespace erlang party_shop
+
+/**
+ * Сервис для получения информации о магазинах по party
+ **/
+service PartyShopService {
+
+    /**
+     * Получение идентификаторов машазинов по party в разрезе тестовой или продовой среды
+     **/
+    list<base.ID> GetShopsIds(1: base.ID party_id, 2: bool is_test)
+
+}
